4f64e055f56adbb6932f60708d2dfc7ba536c405
advancehs <1019753743@qq.com>
2025-05-21 16:46:39 +0800

    Bump version: 0.0.6 → 0.0.7

--- deabook/__init__.py
+++ deabook/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """advancehs"""
 __email__ = "1019753743@qq.com"
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 # from . import CNLS
 # from . import CNLSSDFDDF
 # from . import CNLSSDFDDFweak
